6913a8559bb23a322813b895c588d6e3ed52187b
Maksym Pidlisnyi <p.maxsym.w@gmail.com>
2020-08-02 16:31:07 +0300

    add second dice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
 
     private var diceImage: ImageView? = null
+    private var diceImage1: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,13 +17,14 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         diceImage = findViewById(R.id.dice_image)
+        diceImage1 = findViewById(R.id.dice_image1)
         rollButton.setOnClickListener { rollDice() }
     }
 
     private fun rollDice() {
         Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
         //        val diceImage: ImageView = findViewById(R.id.dice_image)
-        val drawableResource = when ((1..6).random()) {
+        fun getRandomImage() = when ((1..6).random()) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -30,7 +32,8 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        diceImage?.setImageResource(drawableResource)
+        diceImage?.setImageResource(getRandomImage())
+        diceImage1?.setImageResource(getRandomImage())
 
     }
 }
